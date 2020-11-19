ad2c8b3a8564befc08f2a7805531b30632f463b2
Björn Strömberg <bjorn.stromberg@codic.se>
2020-11-19 19:21:27 +0100

    Jag flyttade ut contextet hit för att ha bara ett context per användarsession.

--- DatabaseConnection/API.cs
+++ DatabaseConnection/API.cs
@@ -6,28 +6,26 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatabaseConnection
 {
-    public class API
+    public static class API
     {
+        static Context ctx;
+
+        static API()
+        {
+            ctx = new Context();
+        }
         public static List<Movie> GetMovieSlice(int a, int b)
         {
-            using var ctx = new Context();
             return ctx.Movies.OrderBy(m => m.Title).Skip(a).Take(b).ToList();
         }
         public static Customer GetCustomerByName(string name)
         {
-            using var ctx = new Context();
             return ctx.Customers.FirstOrDefault(c => c.Name.ToLower() == name.ToLower());
         }
         public static bool RegisterSale(Customer customer, Movie movie)
         {
-            using var ctx = new Context();
             try
             {
-                // Här säger jag åt contextet att inte oroa sig över innehållet i dessa records.
-                // Om jag inte gör detta så kommer den att försöka updatera databasens Id och cracha.
-                ctx.Entry(customer).State = EntityState.Unchanged;
-                ctx.Entry(movie).State = EntityState.Unchanged;
-
                 ctx.Add(new Rental() { Date = DateTime.Now, Customer = customer, Movie = movie });
                 return ctx.SaveChanges() == 1;
             }
