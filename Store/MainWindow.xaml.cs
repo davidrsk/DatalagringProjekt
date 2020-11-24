10ecda74b0c0f1862f24f5642b7948b24c4b91ca
Björn Strömberg <bjorn.stromberg@codic.se>
2020-11-24 13:41:07 +0100

    Ännu mera kommentarer men allt kör bra

--- Store/MainWindow.xaml.cs
+++ Store/MainWindow.xaml.cs
@@ -34,13 +34,13 @@ namespace Store
             /*
              * cols = 3, movs = 10
              * 
-             * rows = movs/cols = 3
+             * rows = movs/cols = 3.333
              * 
-             * men vi behöver en extra rad för filmerna som inte fick plats!
+             * vi behöver alltså 4 rader. Vi kan inte bara göra en vanlig heltalsdivision.
              * 
-             * rows = movs/cols + 1 = 4
+             * rows = ceiling(movs/cols) = 4
              */
-            int row_count = State.Movies.Count / column_count + 1;
+            int row_count = (int)Math.Ceiling((double)State.Movies.Count / (double)column_count);
 
             for (int y = 0; y < row_count; y++)
             {
@@ -54,6 +54,7 @@ namespace Store
                 {
                     // Räkna ut vilken film vi ska ploppa in härnäst utifrån mina x,y koordinater
                     int i = y * column_count + x;
+                    // Kolla så att vi inte försöker fylla mer Grid celler än vi har filmrecords.
                     if (i < State.Movies.Count)
                     {
                         // Hämta ett film record
@@ -61,50 +62,55 @@ namespace Store
                         
                         // Försök att skapa en Image Controller(legobit) och
                         // placera den i rätt Grid cell enl. x,y koordinaterna
+                        // Skapa en Image som visar filmomslaget
+                        var image = new Image()
+                        {
+                            Cursor = Cursors.Hand, // Visa en 'click me' hand när man hovrar över bilden
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center,
+                            Margin = new Thickness(4, 4, 4, 4),
+                        };
+                        image.MouseUp += Image_MouseUp;
+
                         try
                         {
-                            // Skapa en Image som visar filmomslaget
-                            var image = new Image()
-                            {
-                                Cursor = Cursors.Hand, // Visa en 'click me' hand när man hovrar över bilden
-                                HorizontalAlignment = HorizontalAlignment.Center,
-                                VerticalAlignment = VerticalAlignment.Center,
-                                Margin = new Thickness(4, 4, 4, 4),
-                            };
                             image.Source = new BitmapImage(new Uri(movie.ImageURL)); // Hämta hem bildlänken till RAM
-                            image.MouseUp += Image_MouseUp;
-
-                            // Lägg till Image i Grid
-                            MovieGrid.Children.Add(image);
-
-                            // Placera in Image i Grid
-                            Grid.SetRow(image, y);
-                            Grid.SetColumn(image, x);
                         }
-                        catch (Exception e) when 
-                            (e is ArgumentNullException || 
-                             e is System.IO.FileNotFoundException || 
+                        catch (Exception e) when
+                            (e is ArgumentNullException ||
+                             e is System.IO.FileNotFoundException ||
                              e is UriFormatException)
                         {
-                            // Om något gick fel så hoppar vi över denna filmen
-                            continue;
+                            // Om något gick fel så lägger vi in en placeholder 
+                            image.Source = new BitmapImage(new Uri("https://wolper.com.au/wp-content/uploads/2017/10/image-placeholder.jpg"));
                         }
+
+                        // Lägg till Image i Grid
+                        MovieGrid.Children.Add(image);
+
+                        // Placera in Image i Grid
+                        Grid.SetRow(image, y);
+                        Grid.SetColumn(image, x);
                     }
                 }
             }
         }
 
+        // Vad som händer när man klickar på en filmikon i appen.
         private void Image_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            // Ta reda på vilken koordinat den klickade bilden har
+            // Ta reda på vilken koordinat den klickade bilden har.
             var x = Grid.GetColumn(sender as UIElement);
             var y = Grid.GetRow(sender as UIElement);
             
-            //
+            // Används koordinaten för att ta reda på vilken motsvarande record det rörde sig om.
             int i = y * MovieGrid.ColumnDefinitions.Count + x;
+            // Lägg valet på minne.
             State.Pick = State.Movies[i];
 
-            if(API.RegisterSale(State.User, State.Pick))
+            // Försök att registrera en uthyrning.
+            if (API.RegisterSale(State.User, State.Pick))
+                // MessageBox är små pop-up fönster som är behändiga för att varna användaren om fel etc.
                 MessageBox.Show("All is well and you can download your movie now.", "Sale Succeeded!", MessageBoxButton.OK, MessageBoxImage.Information);
             else
                 MessageBox.Show("An error happened while buying the movie, please try again at a later time.", "Sale Failed!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
